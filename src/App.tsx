fa4ff8036d894b3e9263cc945b67981dec55f833
Jaden Lewis-Chambers <you@example.com>
2024-11-13 17:12:02 -0500

    font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import * as Toggle from "@radix-ui/react-toggle";
+
 function App() {
-  return <></>;
+  return (
+    <div className="relative flex min-h-screen flex-col items-center">
+      <div className="max-w-[54rem]">
+        <h1 className="text-jetbrains text-white"> test</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
