c589bbfa85881a39423ea8ca76365a9c730f67e8
Jaden Lewis-Chambers <you@example.com>
2024-11-13 23:20:12 -0500

    added bold jetbrains

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="relative mt-[5rem] flex min-h-screen flex-col items-center">
       <div className="min-w-[4rem] max-w-[54rem] p-5">
-        <h1 className="text-jetbrains text-[1.6rem] leading-[2.4rem] text-white">
+        <h1 className="text-[1.6rem] font-bold leading-[2.4rem] text-white">
           {" "}
           Hi, I am Prateek.{" "}
           <span className="font-normal text-gray-500">
