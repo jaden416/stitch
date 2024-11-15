a5e1b21da6888716623d68da179a9d40f2585d25
Jaden Lewis-Chambers <you@example.com>
2024-11-15 17:12:23 -0500

    navigation icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header";
-import s from "./assets/stitch.png";
+import React from "react";
 import { Tooltip } from "./components/Tooltip.tsx";
+import { Icons } from "./components/Icons.tsx";
 import {
   Popover,
   PopoverTrigger,
@@ -8,18 +8,18 @@ import {
   PopoverContent,
 } from "./components/Popover.tsx";
 
-import { Icons } from "./components/Icons.tsx";
-
 function App() {
   const [popoverContent, setPopoverContent] = React.useState<boolean>(false);
-
+  const handleClick = () => {};
   return (
     <nav className="fixed bottom-0 left-1/2 z-50 flex w-full -translate-x-1/2 justify-center pb-[2rem]">
       <Popover open={popoverContent}>
         <PopoverAnchor className="bg-darkgrey shadow-shado relative z-30 flex items-center gap-1.5 rounded-[1.2rem] p-2">
-          {links.map((link) => {
+          {links.map((link, index) => {
             return (
               <Tooltip
+                active={index == 1}
+                handleClick={handleClick}
                 title={link.title}
                 href={link.href}
                 key={link.title}
@@ -31,6 +31,7 @@ function App() {
           })}
           <PopoverTrigger asChild>
             <Tooltip
+              handleClick={handleClick}
               title="Contact"
               active={popoverContent}
               modal={popoverContent}
